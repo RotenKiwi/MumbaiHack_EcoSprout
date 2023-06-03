30fff8914e95ee2027b682d8a07573d219e49e36
RotenKiwi <joshiparth212@gmail.com>
2023-06-03 15:26:01 +0530

    SplashScreen
    Completed splash screen

--- lib/Screens/Intro/SplashScreen.dart
+++ lib/Screens/Intro/SplashScreen.dart
@@ -14,17 +14,47 @@ class _SplashState extends State<Splash> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: InkWell(
-        onTap: (){
-          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Login()));
+        onTap: () {
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => Login()));
         },
         child: Container(
           decoration: BoxDecoration(
             image: DecorationImage(
-              image: AssetImage('assets/images/Splashscreen.jpg'),
+              image: AssetImage('assets/images/Splashscreen-2.jpg'),
               colorFilter: ColorFilter.mode(filter, BlendMode.modulate),
               fit: BoxFit.cover,
             ),
-          )
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/logo.png', scale: 0.85,),
+                  SizedBox(width: 10,),
+                  Text(
+                    'GrowX',
+                    style: TextStyle(
+                      fontFamily: 'SF',
+                      fontSize: 45,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10,),
+              Text(
+                '   Your Gardening Guide',
+                style: TextStyle(
+                  fontFamily: 'SF',
+                  fontSize: 15,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
